23a003539efd9052a3d2a9f7808a281f6d340468
ret2hell <71142616+ret2hell@users.noreply.github.com>
2021-02-08 19:48:10 +0000

    edit

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -7,10 +7,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 20;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "xos4 Terminus:size=8" };
 //static const char *fonts[]          = { "Gohu GohuFont:size=10" };
 //static const char dmenufont[]       = "Gohu GohuFont:size=10";
-static const char dmenufont[]       = "xos4 Terminus:size=8";
+static const char *fonts[]          = { "lucy tewi:size=8" };
+static const char dmenufont[]       = "lucy tewi:size=8";
 static const char col_gray1[]       = "#222222"; // background
 static const char col_gray2[]       = "#444444"; // inactive window border
 static const char col_gray3[]       = "#bbbbbb"; // font
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purp, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termcmd[]  = { "urxvtc", NULL };
 
 
 static Key keys[] = {
@@ -109,6 +109,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
 
 /* button definitions */
