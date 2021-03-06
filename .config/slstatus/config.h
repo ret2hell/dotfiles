3ceca4a23bb8cc52bf6c638f4a430b5bc5b8cf2d
ret2hell <71142616+ret2hell@users.noreply.github.com>
2021-03-06 21:40:04 +0000

    str padding, volume & bat

--- .config/slstatus/config.h
+++ .config/slstatus/config.h
@@ -11,13 +11,16 @@ static const char unknown_str[] = "n/a";
 
 static const struct arg args[] = {
 	/* function, format, argument */
-	{ kernel_release, "%s(gentoo) ~ ", NULL},
+	{ kernel_release, "  %s(gentoo) ~ ", NULL},
 	{ cpu_perc, "cpu: %s%% | ", NULL },
-	//{ ram_perc, "ram: %s%% | ", NULL },
+//	{ ram_perc, "ram: %s%% | ", NULL },
 	{ ram_used, "ram: %s | ", NULL },
 	{ disk_perc, "disk: %s%% | ", "/"    },
 	{ netspeed_rx, "r: %sB/s ", "change_interface" },
 	{ netspeed_tx, "t: %sB/s | ", "change_interface" },
-	{ run_command, "%10s | ", "network.sh" },
+	{ run_command, "%s | ", "network.sh" },
+	{ run_command, "vol: %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+//	{ battery_state, "%s"    , "BAT0" },
+//  { battery_perc,  " %s%% | ", "BAT0" },
 	{ datetime, "%s", "%b %d %R" },
 };
